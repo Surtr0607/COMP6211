a5664367d237b0dab7cc9a6c5a17a95022db98d2
CJM <935734971@qq.com>
2023-05-15 04:32:19 +0800

    allcourse,mycourse update

--- app/src/main/java/com/example/myapplication/ui/course/AllCourse.kt
+++ app/src/main/java/com/example/myapplication/ui/course/AllCourse.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication.ui.course
 
 import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.Toast
 import com.example.myapplication.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.core.UserData
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +35,15 @@ class AllCourse : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var courses: List<Course>
+
+    data class Course(
+        val courseID: String = "",
+        val courseName: String = ""
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -30,13 +52,91 @@ class AllCourse : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_course, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        /*
+                val view = inflater.inflate(R.layout.fragment_all_course, container, false)
+                // 获取 LinearLayout 视图实例
+                val checkboxLayout = view.findViewById<LinearLayout>(R.id.check_box_layout)
+                // 获取 Firestore 实例
+                val firestore = Firebase.firestore
+
+                // 从 Firestore 中读取所有课程的名称
+                firestore.collection("course")
+                    .get()
+                    .addOnSuccessListener { documents ->
+
+                        // 遍历每个文档，创建一个复选框，并将其添加到 LinearLayout 中
+                        for (document in documents) {
+                            val courseName = document.getString("courseName")
+                            if (courseName != null) {
+                                val checkbox = CheckBox(requireContext())
+                                checkbox.text = courseName
+                                checkboxLayout.addView(checkbox)
+                            }
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        // 如果读取过程中发生错误，则在控制台中打印错误消息
+                        Log.e(TAG, "Error getting documents.", exception)
+                    }
+        */
+
+        val view = inflater.inflate(R.layout.fragment_all_course, container, false)
+
+        firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
+
+        val checkboxGroup = view.findViewById<LinearLayout>(R.id.check_box_layout)
+        val addButton = view.findViewById<Button>(R.id.AllCoursesButton)
+
+        // 获取所有课程数据
+        firestore.collection("course")
+            .get()
+            .addOnSuccessListener { result ->
+                courses = result.toObjects(Course::class.java)
+
+                // 创建复选框并显示课程名称
+                for (course in courses) {
+                    val checkbox = CheckBox(context)
+                    checkbox.text = course.courseName
+                    checkboxGroup.addView(checkbox)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Toast.makeText(context, "获取课程数据失败", Toast.LENGTH_SHORT).show()
+            }
+
+        addButton.setOnClickListener {
+            val selectedCourses = mutableListOf<String>()
+
+            // 获取选中的课程ID
+            for (i in 0 until checkboxGroup.childCount) {
+                val checkbox = checkboxGroup.getChildAt(i) as CheckBox
+                if (checkbox.isChecked) {
+                    selectedCourses.add(courses[i].courseID)
+                }
+            }
+
+            // 添加课程ID到当前用户的course字段
+            val currentUser = auth.currentUser
+            if (currentUser != null) {
+                val userRef = firestore.collection("users").document(currentUser.uid)
+                userRef.update("course", selectedCourses)
+                    .addOnSuccessListener {
+                        Toast.makeText(context, "成功添加课程", Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener { exception ->
+                        Toast.makeText(context, "添加课程失败", Toast.LENGTH_SHORT).show()
+                    }
+            } else {
+                Toast.makeText(context, "请先登录", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+        return view
     }
+    data class UserData(val course: List<String>)
 
     companion object {
         /**
--- app/src/main/java/com/example/myapplication/ui/course/MyCourse.kt
+++ app/src/main/java/com/example/myapplication/ui/course/MyCourse.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication.ui.course
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import com.example.myapplication.R
+import com.example.myapplication.ui.exam.ExamList
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,6 +29,14 @@ class MyCourse : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
+    private lateinit var adapter: ArrayAdapter<String>
+
+    private val db = FirebaseFirestore.getInstance()
+    private val currentUser = FirebaseAuth.getInstance().currentUser
+    val listView = view?.findViewById<ListView>(R.id.MycourseList)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -29,15 +44,69 @@ class MyCourse : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_course, container, false)
     }
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // 获取当前登录用户的 UID
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
+
+        // 获取列表视图
+        val listView = view.findViewById<ListView>(R.id.MycourseList)
+
+        // 创建一个空的列表项数组和 courseID 数组
+        val listItems = arrayListOf<String>()
+        val courseIds = arrayListOf<String>()
+
+        // 如果 UID 不为空，则连接 Firestore 并获取当前用户的 course 字段
+        if (uid != null) {
+            db.collection("users").document(uid).get()
+                .addOnSuccessListener { document ->
+                    if (document != null && document.exists()) {
+                        // 获取用户的 course 字段的值
+                        val courseArray = document.get("course") as ArrayList<String>
+                        // 遍历 course 数组并在 Firestore 中查找匹配的数据
+                        for (courseId in courseArray) {
+                            db.collection("course").whereEqualTo("courseID", courseId)
+                                .get()
+                                .addOnSuccessListener { querySnapshot ->
+                                    if (!querySnapshot.isEmpty) {
+                                        // 获取匹配到的数据的 courseName 和 courseID 字段的值
+                                        val courseName = querySnapshot.documents[0].get("courseName") as String
+                                        val courseId = querySnapshot.documents[0].get("courseID") as String
+                                        // 将 courseName 和 courseId 添加到列表项数组和 courseId 数组中
+                                        listItems.add(courseName)
+                                        courseIds.add(courseId)
+                                        // 更新列表视图
+                                        listView.adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, listItems)
+                                    }
+                                }
+                        }
+                    }
+                }
+        }
+
+        // 设置列表视图的点击事件
+        listView.setOnItemClickListener { parent, view, position, id ->
+            // 创建一个 Bundle 对象来传递所选列表项的 courseID 字段的值
+            val bundle = Bundle()
+            bundle.putString("courseId", courseIds[position])
+            // 创建一个新的 Fragment 实例并传递 Bundle 对象
+            val fragment = ExamList()
+            fragment.arguments = bundle
+            // 替换当前 Fragment
+            val fragmentManager = requireActivity().supportFragmentManager
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit()
+        }
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
